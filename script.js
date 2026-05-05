4e132f1976cda65a19123d5e8066d72fe1aa7af8
AChristiam <80561643+AChristiam@users.noreply.github.com>
2026-05-05 18:36:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,59 +4,58 @@ async function getSummary(title) {
   const res = await fetch(url);
   const data = await res.json();
   return data;
-}async function getSummary(title) {
-  const encoded = encodeURIComponent(title.replace(/ /g, "_"));
-  const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encoded}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
 }
 
+function makeWikiSpan(titulo, link, img, desc) {
+  return `
+    <span class="wiki-wrapper">
+      <a href="${link}" target="_blank" class="wiki-link">${titulo}</a>
+      <div class="wiki-tooltip">
+        <div class="wiki-tooltip-inner">
+          ${img ? `<img src="${img}" />` : ""}
+          <div class="wiki-tooltip-text">
+            <strong>${titulo}</strong>
+            <p>${desc}</p>
+          </div>
+        </div>
+      </div>
+    </span>`;
+}
 
 async function gerarFrase() {
   const url = "https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=info&inprop=url&format=json&origin=*";
-
   try {
     const response = await fetch(url);
     const data = await response.json();
-
     const pages = data.query.pages;
     const page = Object.values(pages)[0];
-
     const titulo = page.title;
     const link = page.fullurl;
-
     const summaryData = await getSummary(titulo);
-
     const img = summaryData.thumbnail ? summaryData.thumbnail.source : null;
     const desc = summaryData.extract || "";
 
-   const frase = `
-<p>
-In the search for love, <br>
-I found <span class="wiki-wrapper">
-  <a href="${link}" target="_blank" class="wiki-link">${titulo}</a>
+    const span = makeWikiSpan(titulo, link, img, desc);
 
-  <div class="wiki-tooltip">
-    ${img ? `<img src="${img}" />` : ""}
-    <p>${desc}</p>
-  </div>
-</span> <br><br>
-
-So I started to wonder <br>
-if <span class="wiki-wrapper">
-  <a href="${link}" target="_blank" class="wiki-link">${titulo}</a>
-
-  <div class="wiki-tooltip">
-    ${img ? `<img src="${img}" />` : ""}
-    <p>${desc}</p>
-  </div>
-</span> <br>
-could also be a form of love
-</p>
-`;
+    const frase = `
+      <p>
+        In the search for love, <br>
+        I found ${span} <br><br>
+        So I started to wonder <br>
+        if ${span} <br>
+        could also be a form of love
+      </p>`;
 
     document.getElementById("frase").innerHTML = frase;
+
+    document.querySelectorAll(".wiki-wrapper").forEach(wrapper => {
+      const tooltip = wrapper.querySelector(".wiki-tooltip");
+      wrapper.addEventListener("mouseenter", e => {
+        const rect = wrapper.getBoundingClientRect();
+        tooltip.style.top = (rect.bottom + 8) + "px";
+        tooltip.style.left = Math.min(rect.left, window.innerWidth - 340) + "px";
+      });
+    });
 
   } catch (error) {
     console.error(error);
